f9c01a900993e9a395697c8e78421139f2ec63e5
sourodip19 <sourodipdey19@gmail.com>
2026-03-15 20:23:02 +0530

    feat: add axios instance and auth context
    - configure axios instance with baseURL and withCredentials
    - add signUp, login, logout, getMe API functions
    - create AuthContext with user and loading state
    - wrap app with AuthProvider for global auth access

---
+++ frontend/src/features/auth/hooks/useAuth.js
@@ -0,0 +1,48 @@
+import axios from "axios";
+const api = axios.create({
+  baseUrl: process.env.BACKEND_URL,
+  withCredential: true,
+});
+
+export async function signUp(username, email, password) {
+  try {
+    const response = await api.post("api/user/auth/signup", {
+      username,
+      email,
+      password,
+    });
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function login(email, password) {
+  try {
+    const response = await api.post("api/user/auth/login", {
+      email,
+      password,
+    });
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function logout() {
+  try {
+    const reponse = await api.post("api/user/auth/logout");
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function getMe() {
+  try {
+    const response = await api.get("api/user/auth/getMe");
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+}
