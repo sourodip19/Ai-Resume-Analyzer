32fd94751331c3fec1e49107658adb94c7cd2f5d
sourodip19 <sourodipdey19@gmail.com>
2026-03-16 23:23:50 +0530

    - created api layer - created state layer - created hooks layer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { RouterProvider } from "react-router";
 import { router } from "./app.routes.jsx";
+import { AuthProvider } from "./features/auth/auth.context.js";
 
 const App = () => {
   return (
--- frontend/src/features/auth/pages/Login.jsx
+++ frontend/src/features/auth/pages/Login.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useContext } from "react";
 import "../auth.form.scss";
 import "../../../style/button.scss";
 import { useNavigate, Link } from "react-router";
